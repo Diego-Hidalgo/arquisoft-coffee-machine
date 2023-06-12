8757e1580c0f516b1afe8b2955b06707699a644b
Diego H <d13g01hl@gmail.com>
2023-06-12 09:57:08 -0500

    Initial commit

---
+++ scripts/postgres/coffeemach-user.sql
@@ -0,0 +1,6 @@
+create user cofmachu_Grupo9 with password 'cofmachpwd';
+create database coffeemachine_Grupo9 owner cofmachu_Grupo9;
+grant connect on database coffeemachine_Grupo9 to cofmachu_Grupo9;
+
+-- Connection
+-- psql -d coffeemachine -U p09713_1_2
